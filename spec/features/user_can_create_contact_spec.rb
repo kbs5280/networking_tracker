a610fcdfa15e7b2f602dbae0fcf9470d8a9d1d42
Kris Sparks <kbs5280@gmail.com>
2016-11-30 18:46:52 -0700

    Add test for form for new contact
    Test is failing at route

--- spec/features/user_can_create_contact_spec.rb
+++ spec/features/user_can_create_contact_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User can create contact', type: :feature do
-  scenario 'and views contact on root page' do
+  scenario 'user views contact form on root page' do
     user = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -19,5 +19,33 @@ describe 'User can create contact', type: :feature do
     find_field 'contact[other]'
 
     expect(page).to have_button 'Create contact'
+  end
+
+  scenario 'and views contact on root page' do
+    user = create(:user)
+    contact = create(:contact, user_id: user.id, first_name: 'Alan', last_name: 'Turing', email: 'alan@alan.com', phone: '7205555555', linked_in: 'alan', twitter: 'turing', other: 'other')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit root_path
+
+    expect(page).to have_content 'New contact'
+
+    fill_in 'First name', with: contact.first_name
+    fill_in 'Last name', with: contact.last_name
+    fill_in 'Email', with: contact.email
+    fill_in 'Phone', with: contact.phone
+    fill_in 'LinkedIn', with: contact.linked_in
+    fill_in 'Twitter', with: contact.twitter
+    fill_in 'Other', with: contact.other
+    click_on 'Create contact'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Alan'
+    expect(page).to have_content 'Turing'
+    expect(page).to have_content 'alan@alan.com'
+    expect(page).to have_content '7205555555'
+    expect(page).to have_content 'alan'
+    expect(page).to have_content 'turing'
   end
 end
