36064664bbe634c0e0485f9f623ca694b10779b5
Kris Sparks <kbs5280@gmail.com>
2016-11-30 20:06:25 -0700

    Create contact
    Contact is associated with the user
    Maybe a bug related to redirecting/rendering root_path tin the contacts_controller
    Ready to query the database and render user contacts in the DOM

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,20 @@ class ContactsController < ApplicationController
   end
 
   def create
-
+    user = current_user
+    contact = user.contacts.new(contact_params)
+    if contact.save
+      redirect_to root_path
+      flash[:success] = 'Contact created'
+    else
+      flash.now[:error] = contact.errors.full_messages.join(', ')
+      redirect_to :root_path
+    end
   end
+
+  private
+
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :email, :phone, :linked_in, :twitter, :other, :user_id)
+    end
 end
